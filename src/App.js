539990761a669f3dfd67cfde93f7ee35d5824d76
Pega, Jede Isaiah Maxweil <140310859+STRAT05@users.noreply.github.com>
2025-11-23 16:29:02 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './App.css';
+import InventoryPredictor from './components/InventoryPredictor';
+
+function App() {
+  return (
+    <div className="App">
+      <InventoryPredictor />
+    </div>
+  );
+}
+
+export default App;
